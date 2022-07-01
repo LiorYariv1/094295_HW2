d9f7a664c3d78182e3f148763ccb8c18f1566537
AtarCohen <atar.cohen@gmail.com>
2022-07-01 17:04:24 +0300

    up

--- delete_dir_train_tmp.py
+++ delete_dir_train_tmp.py
@@ -3,5 +3,6 @@ import shutil
 
 if __name__=='__main__':
     train_dir_tmp = os.path.join("data", "train_tmp")
+    print(f'train_dir_tmp={train_dir_tmp}')
     shutil.rmtree(train_dir_tmp)
     print('deleted')
