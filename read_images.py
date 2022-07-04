81be6d85a7732f0a5e385ff998bede8a0116b5d2
LiorYariv1 <lior.ya1994@gmail.com>
2022-07-04 11:55:36 +0300

    X

--- read_images.py
+++ read_images.py
@@ -2,11 +2,12 @@ import os
 import pickle
 import shutil
 #%%
-with open('/home/student/HW2/wandb/run-20220703_042738-fc7q80r7/files/images_by_letter', 'rb') as file:
+with open('/home/student/HW2/wandb/run-20220703_210200-sv9aryh2/files/images_by_letter', 'rb') as file:
     images_by_letter_best_run = pickle.load(file)
 #%%
 train_dir = os.path.join("data", "train")
-final_train_dir = '/home/student/HW2/data/train_final_try'
+val_dir = os.path.join("data", "val")
+final_train_dir = '/home/student/HW2/data_final/data/train'
 for letter,images_list in images_by_letter_best_run.items():
     letter_path = f'{final_train_dir}/{letter}'
     os.makedirs(letter_path, exist_ok=True)
@@ -14,3 +15,5 @@ for letter,images_list in images_by_letter_best_run.items():
     images_list = [f'{letter_dir}/{x}' for x in images_list]
     for image in images_list:
         shutil.copy(image, letter_path)
+final_val_dir =  '/home/student/HW2/data_final/data/val'
+shutil.copytree(val_dir, final_val_dir)
